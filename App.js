b2bed65667b5535e71435c6431f4fcabbc51c807
Amandeepkaur1414 <amandeep.kaur1414@gmail.com>
2023-05-14 16:48:56 +0530

    added jsx and parcel build

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// react Element
+const jsxHeading=(
+    <h1>Hi React</h1>
+);
 
-const header = React.createElement("h1",{id:"heading"},"Hello World form React");
-const root =  ReactDOM.createRoot(document.getElementById('root'));
-root.render(header);
-console.log(header)
+const Title = () =>
+   ( <h1>Hi, Amandeep </h1>);
+
+//react function
+const Heading = () =>(
+ <>
+    <Title/>
+    <h1>Hi React</h1>
+    </>
+);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Heading/>)
