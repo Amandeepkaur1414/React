89539e76047e58d88fb1a2e5271516fdffd821b7
Amandeepkaur1414 <amandeep.kaur1414@gmail.com>
2023-06-04 23:07:15 +0530

    added seacrh filter,reset, andd fetch call

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,63 @@
 import ResturantCard from "./ResturantCard";
-import restList from "../utils/mockData"
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+// import restList from "../utils/mockData"
+import { useEffect, useState } from "react";
 
 const Body =() =>{
-    let restListLocal = restList;
-    const [restListJS,setrestListJS]=useState(restListLocal);
-    return(
+    // let restListLocal = restList;
+    const [restListJS,setrestListJS]=useState([]);
+    const [filteredListJS,setfilteredListJS]=useState([]);
+    const [searchText,setsearchText]=useState("");
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData  = async ()  =>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+        const json =await data.json();
+        console.log("json",json);
+        // optional chaining
+        setrestListJS(json?.data?.cards[2]?.data?.data?.cards);
+        setfilteredListJS(json?.data?.cards[2]?.data?.data?.cards);
+    };
+
+    
+    return restListJS.length == 0 ?<Shimmer/> :(
             <div className="body">
                 <div className="Search">
-                    Search
+                   <input type="text" className="search-box" value={searchText}
+                   onChange={(e)=>{
+                    setsearchText(e.target.value);
+                   }}/> 
+                <div>
+                        <button  className="button-cls" onClick={()=>{
+                            const filterRes = restListJS.filter((ele) => {
+                                console.log(ele.data.name.toLowerCase().includes(searchText));
+                                return ele.data.name.toLowerCase().includes(searchText.toLowerCase());
+                            }
+                            );
+                            setfilteredListJS(filterRes);
+                            }}>Search
+                        </button>
+
+                        <button  className="button-cls" onClick= {()=>{
+                        const filterdData1= restListJS.filter(ele =>{ return ele.data.avgRating >4});
+                        setfilteredListJS(filterdData1);
+                            }}>Top Rated Restaurants</button>
+                
+                        <button  className="button-cls" onClick= {()=>{
+                        setfilteredListJS(restListJS);
+                        setsearchText("");
+                        }}>Reset</button>
+                    </div>
                 </div>
-                <button className="btn-rating" onClick= {()=>{
-                    let filterdData= restListJS.filter(ele =>{ return ele.data.avgRating >4});
-                    setrestListJS(filterdData);
-                }}>Top Rated Restaurants</button>
-                <div className="res-container">
-                   { restListJS.map((restData) =>(
-                        <ResturantCard key ={restData.data.id} restObj={restData}/>
-                      ))
-                      }
-                </div>
+               
+                    <div className="res-container">
+                    { filteredListJS.map((restData) =>(
+                            <ResturantCard key ={restData.data.id} restObj={restData}/>
+                        ))
+                        }
+                    </div>
 
             </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
-
-import logo from '../../assets/baby-food-logo.jpg'; 
+import {useState} from 'react';
+import logo from '../../assets/food-logo.jpg'; 
 const Header =() =>{
+    const  [btnName, setbtnName]= useState("Login");
     return (
     <div className="header">
         <div className="logo-container">
@@ -20,6 +21,13 @@ const Header =() =>{
                 <li>
                     Cart
                 </li>
+               
+                    <button className="button-login" onClick={
+                        () =>{
+                            btnName =="Login"?  setbtnName("Logout") :setbtnName("Login");
+                        }
+                    }>{btnName}</button>
+                
             </ul>
         </div>
     </div>
